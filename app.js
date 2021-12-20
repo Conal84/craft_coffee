cb406bf4fadff0b6cf8848c9e59f46a3f4716971
Conal Walsh <conal.walsh1@gmail.com>
2021-12-20 20:30:36 +0000

    Started home page front end

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const globalErrorHandler = require("./controllers/errorController");
 const viewRouter = require("./routes/viewRoutes");
@@ -11,6 +13,19 @@ app.enable("trust proxy");
 // Set Pug template engine
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+// 1) GLOBAL MIDDLEWARES
+app.use(cors()); // Middleware function that adds CORS headers to our response for simple requests GET, POST
+
+app.options("*", cors()); // for complex requests PATCH, DELETE, etc this is needed to respond to pre-flight
+
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Middleware HTTP request logger for development
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // 2) ROUTES
 app.use("/", viewRouter);
