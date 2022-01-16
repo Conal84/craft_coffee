af5b096b26d2ec610df3a3524ee6410a0a269800
Conal Walsh <conal.walsh1@gmail.com>
2022-01-16 19:14:18 +0000

    Added sidebar navigation and link items

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -1,6 +1,13 @@
 let burger = document.querySelector(".nav__burger");
-let sidenav = document.querySelector(".nav__sidenav");
+let sidenav = document.querySelector(".sidenav");
+let learn = document.querySelector(".sidenav__list__item__text");
+let learn_dropdown = document.querySelector(".sidenav__list__item__drop");
 
 burger.addEventListener("click", function () {
   sidenav.classList.toggle("hide");
+});
+
+learn.addEventListener("click", function (e) {
+  learn_dropdown.classList.toggle("hide");
+  e.stopPropagation();
 });
