c45d8689d647bc49a794e71777eb46ff72a4a344
Conal Walsh <conal.walsh1@gmail.com>
2022-01-18 19:55:22 +0000

    Added transition to sub menu drop down

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -4,10 +4,15 @@ let learn = document.querySelector(".sidenav__list__item__text");
 let learn_dropdown = document.querySelector(".sidenav__list__item__drop");
 
 burger.addEventListener("click", function () {
-  sidenav.classList.toggle("hide");
+  sidenav.classList.toggle("sidenav__active");
 });
 
+// learn.addEventListener("click", function (e) {
+//   learn_dropdown.classList.toggle("hide");
+//   e.stopPropagation();
+// });
+
 learn.addEventListener("click", function (e) {
-  learn_dropdown.classList.toggle("hide");
+  learn_dropdown.classList.toggle("sidenav__list__item__drop__active");
   e.stopPropagation();
 });
