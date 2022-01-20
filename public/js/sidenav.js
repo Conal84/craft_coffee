595fb1ac9ebdc6610e83bee46b49e1c797f35368
Conal Walsh <conal.walsh1@gmail.com>
2022-01-20 19:45:42 +0000

    Added search form to sidenav

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -2,6 +2,7 @@ let burger = document.querySelector(".nav__burger");
 let sidenav = document.querySelector(".sidenav");
 let caret = document.querySelector(".sidenav__caret");
 let learn_dropdown = document.querySelector(".sidenav__list__item__drop");
+let sidenav_search = document.querySelector(".sidenav__form--container");
 
 burger.addEventListener("click", function () {
   sidenav.classList.toggle("sidenav__active");
@@ -10,5 +11,10 @@ burger.addEventListener("click", function () {
 caret.addEventListener("click", function (e) {
   learn_dropdown.classList.toggle("sidenav__list__item__drop__active");
   caret.classList.toggle("sidenav__caret--rotate");
+  e.stopPropagation();
+});
+
+sidenav_search.addEventListener("click", function (e) {
+  e.preventDefault();
   e.stopPropagation();
 });
