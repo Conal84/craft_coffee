40513cba0d624c7f4e7ac178bac4a6b02f073154
Conal Walsh <conal.walsh1@gmail.com>
2022-07-17 12:56:10 +0100

    Added routes for DB query by date added and by roast level in ascending and descending order

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const factory = require("./handlerFactory");
 const catchAsync = require("../utils/catchAsync");
+const { getProducts } = require("./viewsController");
 
 // Create a product
 exports.createProduct = factory.createOne(Product);
@@ -17,14 +18,71 @@ exports.updateProduct = factory.updateOne(Product);
 // Delete a product
 exports.deleteProduct = factory.deleteOne(Product);
 
-// Get all by roast value, 1, 2, 3, 4 or 5
-// router.get("/roast-level/:value", productController.getByRoastValue);
-exports.getByRoastValue = catchAsync(async (req, res, next) => {
-  const { value } = req.params;
-  const docs = await Product.find({ roast: value });
+// Get all product variants
+exports.getAllVariants = catchAsync(async (req, res, next) => {
+  const docs = await Product.find({}, { variants: 1 });
 
   res.status(200).json({
     status: "success",
+    data: {
+      data: docs,
+    },
+  });
+});
+
+// Get all by roast value, 1, 2, 3, 4 or 5
+// Roast value = desc => high to low
+// Roast value = asc => low to high
+// router.get("/roast-level/:value", productController.getByRoastValue);
+exports.getByRoastValue = catchAsync(async (req, res, next) => {
+  const { value } = req.params;
+  let docs;
+
+  if (value === "desc") {
+    docs = await Product.find().sort({ roast: value });
+  } else if (value === "asc") {
+    docs = await Product.find().sort({ roast: value });
+  } else {
+    docs = await Product.find({ roast: value });
+  }
+
+  res.status(200).json({
+    status: "success",
+    results: docs.length,
+    data: {
+      data: docs,
+    },
+  });
+});
+
+// Get all by size ascending / descending
+//router.get("/size/:value", productController.getBySize);
+exports.getBySize = catchAsync(async (req, res, next) => {
+  const { value } = req.params;
+
+  //const docs = await Product.find().sort({ size: value });
+  // const docs = await Product.find({}, { variants: 1 }).sort({ size: value });
+
+  res.status(200).json({
+    status: "success",
+    value: value,
+    results: docs.length,
+    data: {
+      data: docs,
+    },
+  });
+});
+
+// Get all by date added ascending / descending
+//router.get("/date/:value", productController.getByDate);
+exports.getByDate = catchAsync(async (req, res, next) => {
+  const { value } = req.params;
+
+  const docs = await Product.find().sort({ dateAdded: value });
+
+  res.status(200).json({
+    status: "success",
+    value: value,
     results: docs.length,
     data: {
       data: docs,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,6 +11,9 @@ router.post("/", productController.createProduct);
 // Read all products
 router.get("/", productController.getAllProducts);
 
+// Read all product variants
+router.get("/variants", productController.getAllVariants);
+
 // Read one product by Id
 router.get("/:id", productController.getProduct);
 
@@ -21,6 +24,13 @@ router.patch("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
 // Read all products by roast level, 1, 2, 3, 4 or 5
+// Read products and sort by ascending or descending order
 router.get("/roast-level/:value", productController.getByRoastValue);
+
+// Read all products and sort by ascending / descending size
+router.get("/size/:value", productController.getBySize);
+
+// Read all products and sort by ascending / descending date added
+router.get("/date/:value", productController.getByDate);
 
 module.exports = router;
